a525d644c4067d77d3983c93e1c0ff8394e66bb8
jeleshkafff <jeleshkafff@gmail.com>
2022-02-08 17:53:47 +0300

    l

---
+++ 1.js
@@ -0,0 +1,10 @@
+let styles = ["Jazz", "Blues"];
+console.log(styles);
+styles.push("Rock and Roll");
+console.log(styles);
+styles[Math.floor((styles.length - 1) / 2)] = "Classic"; 
+console.log(styles);
+console.log ([styles.shift()] );
+console.log(styles);
+styles.unshift("Rap ", "Reggae");
+console.log(styles);
---
+++ 2.js
@@ -0,0 +1,97 @@
+// //Задание 2
+// let i = 1;
+// while (i <= 100) {
+// 	console.log(i)
+//     i++;
+// }
+// //Задание 3
+// let i = 100;
+// while (i >=  1) {
+// 	console.log(i)
+//     i--;
+// }
+// //Задание 4
+// let i = 2;
+// while (i <= 100) {
+// 	console.log(i)
+//     i+=2;
+// }
+// //Задание 5
+// let mass = [];
+// let i = 0
+// while ( i < 10 ) {
+// 	mass[i] = 'x';
+//     i++;
+// }
+// console.log(mass);
+// //Задание 6
+// let mass = [];
+// let i = 1;
+// while ( i <= 10) {
+// 	mass.push(i);
+//     i++
+// }
+// console.log(mass);
+// //Задание 7
+// let mass = [];
+// let i = 0;
+// while ( i < 10 ) {
+// 	mass.push(Math.random().toFixed(2));
+//     i++
+// }
+// console.log(mass);
+// //Задание 8
+// let mass = [];
+// let i = 0;
+// while ( i < 10 ) {
+// 	mass.push(Math.round(Math.random() * 10) + 1);
+//     i++
+// }
+// console.log(mass);
+// //Задание 9
+// let mass = [1,3,4,54,-45,345,234];
+// let i = 0;
+// while ( i < mass.length) {
+// 	if(mass[i] > 0 && mass[i] < 10) {
+// 		console.log(mass[i]);
+// 	}
+//     i++
+// }
+// //Задание 10
+// let mass = [1,2,3,4];
+// let i =0
+// while ( i < mass.length ) {
+// 	if(mass[i] == 5) {
+// 		console.log('Есть!')
+//         break
+// 	}
+//     i++  
+// }
+// //Задание 11
+// let mass = [12,32,1];
+// let summ = 0;
+// let i = 0;
+// while ( i < mass.length) {
+// 	summ += mass[i];
+//     i++
+// }
+// console.log(summ);
+// //Задание 12
+// let mass = [1,3];
+// let summ = 0;
+// let i = 0;
+// while ( i < mass.length) {
+// 	summ += mass[i] * mass[i];
+//     i++
+// }
+// console.log(summ);
+// //Задание 13
+// let mass = [3,3,3];
+// let summ = 0;
+// let i = 0;
+// while ( i < mass.length) {
+// 	summ += mass[i];
+//     i++
+// }
+// let result = summ / mass.length;
+// console.log(result);
